4e6a4d3d3e28b2d10881065e2759e4206c440ada
Maria Reedstrom <maria.reedstrom@gmail.com>
2021-12-06 13:14:21 -0600

    commit tree bdd7d99e21c0f2ca811d633536f44d65d0edc776

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+class Polygon {
+  /**
+   *
+   * @param {number[]} sides
+   */
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((accumulator, current) => accumulator + current);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    return (
+      this.sides[0] + this.sides[1] > this.sides[2] &&
+      this.sides[0] + this.sides[2] > this.sides[1] &&
+      this.sides[1] + this.sides[2] > this.sides[0]
+    );
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return this.sides.every((val, i, arr) => val === arr[0]);
+  }
+
+  get area() {
+    return this.sides[0] ** 2;
+  }
+}
